a0d924bf455cb9b092fb846c73d6ee93a277e7a4
sakshi <sakshianandk@gmail.com>
2021-08-10 17:53:58 +0530

    UseStateBasics

---
+++ src/tutorial/UseStatebasic.js
@@ -0,0 +1,13 @@
+//rafce and hit enter
+import React,{useState} from 'react'
+
+const UseStateBasic = () => {
+    console.log(useState("Hola world"));
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UseStateBasic
---
+++ src/tutorial/useStateErrorEx.js
@@ -0,0 +1,26 @@
+import React , {useState} from 'react'
+
+function UseStateErrorEx() {
+    let title="Random Title"
+
+    const handleClick = ()=>{
+        console.log(title);
+        title="My name is Sakshi";
+    }
+
+    return (
+        <React.Fragment>
+            <h2>{title}</h2>
+            <button type="button" className="btn" onClick={handleClick} >Change Title</button>
+        </React.Fragment>
+
+
+        // <div>
+        //     <h2>Use State Error Example</h2>
+        // </div>
+
+        
+    )
+}
+
+export default UseStateErrorEx
