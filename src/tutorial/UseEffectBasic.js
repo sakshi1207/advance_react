4fab3680ef40968c684be32f23a025f676d3ea81
sakshi <sakshianandk@gmail.com>
2021-08-11 14:34:26 +0530

    UseEffectBasic dependency

--- src/tutorial/UseEffectBasic.js
+++ src/tutorial/UseEffectBasic.js
@@ -5,8 +5,10 @@ function UseEffectBasic() {
     const [value,setValue] = useState(0);
     useEffect(() =>{
         console.log('call useEffect');
+        if(value>1){ //hooks can be called inside the effect but it cannot called conditionally outside the efect
         document.title = `New Messages(${value})`;
-    });
+        }
+    },[value]); // this is dependency,, if we not declare dependency it will not render inside the effect, it willl only render once
 console.log('render component');
 
     return (
