753e85e2a789f686e9bcfb3f013f269bb9e46de7
sakshi <sakshianandk@gmail.com>
2021-08-11 10:17:58 +0530

    UseStateBasic hooks advance by passing arrays(removing one by one)

--- src/tutorial/UseStateArray.js
+++ src/tutorial/UseStateArray.js
@@ -3,6 +3,10 @@ import { data } from './data';
 
 function UseStateArray() {
     const [people,setPeople] = React.useState(data)
+    const removeItem = (id) => {
+        let newPeople = people.filter((person) => person.id != id)
+        setPeople(newPeople)
+    }
     return (
         <>
         {
@@ -13,6 +17,7 @@ function UseStateArray() {
                 return (
                     <div key={id} className="item">
                         <h4>{name}</h4>
+                        <button onClick={() => removeItem(id)}>Remove him</button>
                     </div>
                     
                 )
