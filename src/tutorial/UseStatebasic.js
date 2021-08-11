7b797a07e88c67033595f6292569f01c9906bfd1
sakshi <sakshianandk@gmail.com>
2021-08-11 09:32:32 +0530

    UseStateBasic hooks

--- src/tutorial/UseStatebasic.js
+++ src/tutorial/UseStatebasic.js
@@ -1,11 +1,34 @@
 //rafce and hit enter
 import React,{useState} from 'react'
 
-const UseStateBasic = () => {
-    console.log(useState("Hola world"));
+//spme basic rules of hooks
+//useState is one of the hooks
+//component name must be in upper case
+//must be in function/component body
+//can not call conditionally
+//react fragment can also be written as <> </>
+
+
+function UseStateBasic() {
+    // console.log(useState);
+    // console.log(useState("Hola world"));
+    const [day, setDay] = useState("Today is sunny day");
+    const handleClick = () => {
+        if(day ==="Today is sunny day"){
+            setDay("Today is Rainy Day")
+        }
+        else{   
+        setDay("Today is sunny day")
+        }
+    };
+
     return (
+        
         <div>
-            
+            <div>
+            <h1>{day}</h1>
+            <button type="button" className="btn" onClick={handleClick} >Change Day</button>
+            </div>
         </div>
     )
 }
